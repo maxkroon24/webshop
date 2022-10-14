7fc9ddb9c21d018bea9e0a4318a96bf7fce9b289
Max Kroon <kroonmax@icloud.com>
2022-10-14 18:38:52 +0200

    description added

--- creativecases/src/pages/ItemInfo.jsx
+++ creativecases/src/pages/ItemInfo.jsx
@@ -3,7 +3,10 @@ import { useLocation } from "react-router-dom";
 function ItemInfo() {
     const { state } = useLocation();
   return (
+    <>
     <div>{state.title}</div>
+    <div className="description">{state.description}</div>
+    </>
   )
 }
 
