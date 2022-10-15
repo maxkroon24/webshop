05a072df223c227cadf8af0e06695645e5b344f6
Max Kroon <kroonmax@icloud.com>
2022-10-15 12:11:25 +0200

    added explaination to contact page

--- creativecases/src/pages/Contact.jsx
+++ creativecases/src/pages/Contact.jsx
@@ -4,7 +4,12 @@ function Contact() {
   return (
     <>
     <Layout />
-    <div>Contact</div>
+    <section className="contact-wrapper">
+      <div className="explain-wrapper">
+      <h1 className="contact-title">Contact us</h1>
+      <p className="explain-contact">Provide feedback or a problem and we'll solve it as soon as possible</p>
+      </div>
+    </section>
     </>
   )
 }
