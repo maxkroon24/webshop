be28cf7460906828eb052bcb7127299643020daf
Max Kroon <kroonmax@icloud.com>
2022-10-18 20:47:40 +0200

    added item info page

--- creativecases/src/pages/ItemInfo.jsx
+++ creativecases/src/pages/ItemInfo.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useLocation } from "react-router-dom";
+import Layout3 from "../pages/Layout3";
 function ItemInfo() {
     const { state } = useLocation();
+    const [counter, setCounter]= useState(1)
+    function getCount(result, e) {
+      e.preventDefault()
+      if(result === "subtract" && counter > 1) {
+        setCounter(counter - 1)
+      }
+      if(result === "add") {
+        setCounter(counter + 1)
+      }
+    }
   return (
     <>
-    <div>{state.title}</div>
-    <div>{state.description}</div>
-    <div>{state.price}</div>
+    <Layout3 />
+    <div className="item-page-wrapper">
+      <div className="item-img-wrapper">
+        <img src={state.image} alt="" />
+      </div>
+      <div className="item-info-wrapper">
+        <h1 className="item-info-title">{state.title}</h1>
+        <p className="item-info-price">${state.price}</p>
+        <button className="item-info-options">Choose your model</button>
+        <button className="add-to-cart">Add to cart</button>
+        <div className="counter">
+        <button className="min-counter" onClick={(e) => getCount("subtract", e)}>-</button>
+        <div className="counter-result">{counter}</div>
+        <button className="plus-counter" onClick={(e) => getCount("add", e)}>+</button>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
---
+++ creativecases/src/pages/Layout3.jsx
@@ -0,0 +1,38 @@
+import { Outlet, Link } from "react-router-dom";
+import { useState } from "react";
+const Layout3 = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+  return (
+    <>
+      <nav className="nav" style={{backgroundColor: 'black'}}>
+        
+        <button className="hamburger" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <li className="logo"><Link to="/" className="a1">Creative</Link></li>
+        <ul className={isNavExpanded ? "links expanded" : "links"}>
+          <li className="link"><Link to="/products" className="a">Products</Link></li>
+          <li className="link"><Link to="/about" className="a">About</Link></li>
+          <li className="link"><Link to="/contact" className="a">Contact</Link></li>
+          <li className="link"><Link to="/" className="a2">Cart</Link></li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout3;
